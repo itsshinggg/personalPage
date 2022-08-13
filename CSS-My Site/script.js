6bacee270ddd19d31485d755d29a0c1b95a1dd77
shingggk <kiseshingo@cityuniversity.edu>
2022-08-12 21:49:22 -0700

    Added JS file

--- CSS-My Site/script.js
+++ CSS-My Site/script.js
@@ -1,5 +1,27 @@
-'use strict';
+"use strict";
 
-document.querySelector('.btn').addEventListener("mouseover", function(){
-  console.log("we did it!");
+
+document.querySelector("h1").addEventListener("mouseover", function(){
+  console.log("Yes.")
+  document.querySelector("h1").style.color = "#3AB0FF";
 })
+
+document.querySelector(".linkedin").addEventListener('mouseover', function(){
+    document.querySelector(".linkedin").style.width = "7%";
+  });
+
+document.querySelector(".linkedin").addEventListener('mouseleave', function(){
+  document.querySelector(".linkedin").style.width = "5%";
+})
+
+document.querySelector(".github").addEventListener('mouseover', function(){
+    document.querySelector(".github").style.width = "7%";
+  });
+
+document.querySelector(".github").addEventListener('mouseleave', function(){
+  document.querySelector(".github").style.width = "5%";
+});
+
+
+//TODO
+  // reduce the repetition
