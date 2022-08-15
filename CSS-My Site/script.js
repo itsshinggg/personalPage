7b0c51c075ab951c76d41fdc13abb4009e7dae9a
shingggk <kiseshingo@cityuniversity.edu>
2022-08-14 17:20:15 -0700

    + resume & overlay functions

--- CSS-My Site/script.js
+++ CSS-My Site/script.js
@@ -1,4 +1,6 @@
 "use strict";
+
+// name color change
 const myName = document.querySelector("h1");
 
 myName.addEventListener("mouseover", function () {
@@ -9,6 +11,23 @@ myName.addEventListener("mouseleave", function () {
   myName.style.color = "#ffa500";
 });
 
+const modal = document.querySelector(".modal");
+const overlay = document.querySelector(".overlay");
+
+// resume pop up
+document.querySelector(".resume-button").addEventListener("click", function () {
+  console.log("clicked!");
+  modal.classList.remove("hidden");
+  overlay.classList.remove("hidden");
+});
+
+// resume close
+document.querySelector(".close-modal").addEventListener("click", function () {
+  modal.classList.add("hidden");
+  overlay.classList.add("hidden");
+});
+
+// contact logo size change
 const logos = document.querySelectorAll(".logo");
 
 for (let i = 0; i < logos.length; i++)
